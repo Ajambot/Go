1989718ad6ec5770d1fd9886c518528dababe312
Martin Morales <mmorale5@lakeheadu.ca>
2025-08-24 14:55:18 -0400

    L5 Chapter 1 of httpfromtcp solution

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -14,29 +14,40 @@ func main() {
 		return
 	}
 
-	curString := ""
-	for {
-		buf := make([]byte, 8)
+	ch := getLinesChannel(file)
 
-		n, err := file.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		buf = buf[:n]
-		slice := strings.Split(string(buf), "\n")
-		if len(slice) > 1 {
-			curString += slice[0]
-			fmt.Printf("read: %s\n", curString)
-			curString = slice[1]
-		} else {
-			curString += string(buf)
-		}
-	}
-	if curString != "" {
-		fmt.Printf("read: %s\n", curString)
+	for l := range ch {
+		fmt.Println("read:", l)
 	}
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
 
+	go func() {
+		defer close(ch)
+		defer f.Close()
+		curString := ""
+		for {
+			buf := make([]byte, 8)
+
+			n, err := f.Read(buf)
+			if err == io.EOF {
+				break
+			}
+			buf = buf[:n]
+			slice := strings.Split(string(buf), "\n")
+			if len(slice) > 1 {
+				curString += slice[0]
+				ch <- curString
+				curString = slice[1]
+			} else {
+				curString += string(buf)
+			}
+		}
+		if curString != "" {
+			ch <- curString
+		}
+	}()
+	return ch
 }
